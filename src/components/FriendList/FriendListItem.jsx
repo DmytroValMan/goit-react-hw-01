5b19edabddf9bb4eda788b40197013bfc29f56bc
DmytroValMan <dmitri.m2011@gmail.com>
2025-03-27 18:48:14 +0200

    React hw-1

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import { SiGeeksforgeeks } from "react-icons/si";
+import css from "./FriendList.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div>
+      <img className={css.img} src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
+      <p className={`${css.status} ${isOnline ? css.isOnline : css.isOffline}`}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
